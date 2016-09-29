87c904c44bcab9a7b7a4fba06b6731a14d8cc9db
lucasfigueiredoweb <lucasfigueiredoweb@gmail.com>
2016-09-29 13:19:22 -0300

    Spring Boot

---
+++ src/main/java/SpringFinal/models/Users.java
@@ -0,0 +1,30 @@
+package SpringFinal.models;
+
+public class Users {
+	
+	private int id;
+	private String nome;
+	private String address;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+
+}
